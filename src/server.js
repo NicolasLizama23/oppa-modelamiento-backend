d340f22690bfd7b3d8fbb8b338dc3c1e1787fa35
Nicolás Lizama <TU_CORREO@dominio.com>
2026-02-10 00:45:40 -0300

    update, server.js: se agregan rutas para servir la UI de cupones

--- src/server.js
+++ src/server.js
@@ -3,10 +3,27 @@ import cors from "cors";
 import couponsRouter from "./routes/coupons.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import seedRouter from "./routes/seed.routes.js";
-
+// librerías Node para resolver rutas en ES Modules
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+
+// __dirname compatible con ES Modules
+// (porque en "type": "module" no existe __dirname nativo)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// servir UI (HTML/CSS/JS) como estáticos bajo /admin
+// Esto permite pedir: /admin/assets/css/admin.css, /admin/assets/js/coupons.js, etc.
+app.use("/admin", express.static(path.join(__dirname, "../ui")));
+
+// endpoint que entrega la pantalla de gestión de cupones
+// URL final: http://localhost:3000/admin/coupons
+app.get("/admin/coupons", (req, res) => {
+  res.sendFile(path.join(__dirname, "../ui/pages/coupons.html"));
+});
 
 app.use(cors());
 // Middleware para parsear JSON
